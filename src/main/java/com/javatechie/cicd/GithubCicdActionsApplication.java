edd3f3f964591c586ed77cbd4beae22acad8747e
Devsharun <113292715+Devsharun@users.noreply.github.com>
2023-02-14 15:22:24 +0530

    Update GithubCicdActionsApplication.java

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to javatechie !";
+		return "Welcome to Sharun's land of Happiness :)!";
 	}
 
 //	echo "# github-actions-example" >> README.md
